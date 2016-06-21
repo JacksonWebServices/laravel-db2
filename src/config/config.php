1e5433a37ae54d17d3118c54b053d3fbf5af943d
Joshua Chandler <joshchandler88@gmail.com>
2016-06-21 16:37:26 -0400

    Remove all unnecessary settings

--- src/config/config.php
+++ src/config/config.php
@@ -2,8 +2,6 @@
 
 return [
     
-    
-    
     'connections' => [
 
         'odbc' => [
@@ -15,64 +13,19 @@ return [
             'password'             => env('DB2_pw'),
             //Server settings
             'database'             => env('DB2_ODBC_name'),
-            'prefix'               => '',
             'schema'               => env('DB2_default_table'),
-            'signon'               => 3,
-            'ssl'                  => 0,
             'commitMode'           => 2,
             'connectionType'       => 0,
-            'defaultLibraries'     => '',
-            'naming'               => 0,
-            'unicodeSql'           => 0,
-            // Format settings
-            'dateFormat'           => 5,
-            'dateSeperator'        => 0,
-            'decimal'              => 0,
-            'timeFormat'           => 0,
-            'timeSeparator'        => 0,
-            // Performances settings
-            'blockFetch'           => 1,
-            'blockSizeKB'          => 32,
+            'defaultLibraries'     => 'QGPL',
+            'naming'               => 1,
             'allowDataCompression' => 1,
-            'concurrency'          => 0,
-            'lazyClose'            => 0,
-            'maxFieldLength'       => 15360,
-            'prefetch'             => 0,
-            'queryTimeout'         => 1,
-            // Modules settings
             'defaultPkgLibrary'    => 'QGPL',
             'defaultPackage'       => 'A/DEFAULT(IBM),2,0,1,0',
-            'extendedDynamic'      => 1,
-            // Diagnostic settings
-            'QAQQINILibrary'       => '',
-            'sqDiagCode'           => '',
-            // Sort settings
-            'languageId'           => 'ENU',
-            'sortTable'            => '',
-            'sortSequence'         => 0,
-            'sortWeight'           => 0,
-            'jobSort'              => 0,
-            // Conversion settings
+            'extendedDynamic'      => 0,
             'allowUnsupportedChar' => 0,
-            'ccsid'                => 1208,
-            'graphic'              => 0,
             'forceTranslation'     => 0,
-            // Other settings
-            'allowProcCalls'       => 0,
-            'DB2SqlStates'         => 0,
-            'debug'                => 0,
-            'trueAutoCommit'       => 0,
-            'catalogOptions'       => 3,
             'libraryView'          => 0,
-            'ODBCRemarks'          => 0,
-            'searchPattern'        => 1,
-            'translationDLL'       => '',
-            'translationOption'    => 0,
-            'maxTraceSize'         => 0,
-            'multipleTraceFiles'   => 1,
             'trace'                => 0,
-            'traceFilename'        => '',
-            'extendedColInfo'      => 0,
             'options'  => [
                 PDO::ATTR_CASE => PDO::CASE_LOWER,
                 PDO::ATTR_EMULATE_PREPARES => false,
@@ -84,100 +37,3 @@ return [
     ]
 
 ];
-
-
-/*
-
-    // default settings
-    
-    'connections' => [
-
-        'ibmi' => [
-            'driver'               => 'odbc' / 'ibm',
-            'driverName'           => '{IBM i Access ODBC Driver}' / '{iSeries Access ODBC Driver}',
-             // General settings
-            'host'                 => 'server',
-            'username'             => '',
-            'password'             => '',
-            //Server settings
-            'database'             => 'WRKRDBDIRE entry',
-            'prefix'               => '',
-            'schema'               => 'default schema',
-            'signon'               => 3,
-            'ssl'                  => 0,
-            'commitMode'           => 2,
-            'connectionType'       => 0,
-            'defaultLibraries'     => '',
-            'naming'               => 0,
-            'unicodeSql'           => 0,
-            // Format settings
-            'dateFormat'           => 5,
-            'dateSeperator'        => 0,
-            'decimal'              => 0,
-            'timeFormat'           => 0,
-            'timeSeparator'        => 0,
-            // Performances settings
-            'blockFetch'           => 1,
-            'blockSizeKB'          => 32,
-            'allowDataCompression' => 1,
-            'concurrency'          => 0,
-            'lazyClose'            => 0,
-            'maxFieldLength'       => 15360,
-            'prefetch'             => 0,
-            'queryTimeout'         => 1,
-            // Modules settings
-            'defaultPkgLibrary'    => 'QGPL',
-            'defaultPackage'       => 'A/DEFAULT(IBM),2,0,1,0',
-            'extendedDynamic'      => 1,
-            // Diagnostic settings
-            'QAQQINILibrary'       => '',
-            'sqDiagCode'           => '',
-            // Sort settings
-            'languageId'           => 'ENU',
-            'sortTable'            => '',
-            'sortSequence'         => 0,
-            'sortWeight'           => 0,
-            'jobSort'              => 0,
-            // Conversion settings
-            'allowUnsupportedChar' => 0,
-            'ccsid'                => 1208,
-            'graphic'              => 0,
-            'forceTranslation'     => 0,
-            // Other settings
-            'allowProcCalls'       => 0,
-            'DB2SqlStates'         => 0,
-            'debug'                => 0,
-            'trueAutoCommit'       => 0,
-            'catalogOptions'       => 3,
-            'libraryView'          => 0,
-            'ODBCRemarks'          => 0,
-            'searchPattern'        => 1,
-            'translationDLL'       => '',
-            'translationOption'    => 0,
-            'maxTraceSize'         => 0,
-            'multipleTraceFiles'   => 1,
-            'trace'                => 0,
-            'traceFilename'        => '',
-            'extendedColInfo'      => 0,
-            'options'  => [
-                PDO::ATTR_CASE => PDO::CASE_LOWER,
-                PDO::ATTR_EMULATE_PREPARES => false,
-                PDO::ATTR_PERSISTENT => false
-            ],
-            'toolkit' => [
-                'XMLServiceLib' => 'XMLSERVICE',
-                'debug' => false,
-                'debugLogFile' => storage_path() . '/logs/toolkit.log',
-                'InternalKey' => '/tmp/' . 'Toolkit_' . app()->environment() . '_' . rand(1, 10),
-                'stateless' => false,
-                'plugSize' => '4K',
-                'ccsidBefore' => "819/1147",
-                'ccsidAfter' => "1147/819",
-                'useHex' => true
-            ]
-        ],
-
-    ]
-    
-*/
-
